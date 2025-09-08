1fb13eac4a48180c635d5e95a1822aa481ca9c01
Ahmd Fahs <afahs@c1r1s1.42beirut.com>
2025-09-08 20:27:47 +0300

    /philo/src/utils.c [done]

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,8 +6,62 @@
 /*   By: afahs <afahs@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 15:56:14 by afahs             #+#    #+#             */
-/*   Updated: 2025/09/08 16:04:45 by afahs            ###   ########.fr       */
+/*   Updated: 2025/09/08 20:26:47 by afahs            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+long	get_time(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	precise_usleep(long microseconds)
+{
+	long	start;
+	long	elapsed;
+	long	rem;
+
+	start = get_time();
+	while (1)
+	{
+		elapsed = get_time() - start;
+		rem = microseconds - elapsed;
+		if (rem <= 0)
+			break ;
+		if (rem > 1000)
+			usleep(rem / 2);
+		else
+		{
+			while (get_time() - start < microseconds)
+				;
+		}
+	}
+}
+
+int	is_dead(t_data *data)
+{
+	int	dead;
+
+	pthread_mutex_lock(&data->dead_mutex);
+	dead = data->dead_flag;
+	pthread_mutex_unlock(&data->dead_mutex);
+	return (dead);
+}
+
+void	print_status(t_philo *philo, char *status)
+{
+	long	time;
+
+	if (is_dead(philo->data))
+		return ;
+	pthread_mutex_lock(&philo->data->print_mutex);
+	time = get_time() - philo->data->start_time;
+	if (!is_dead(philo->data))
+		printf("%ld %d %s\n", time, philo->id, status);
+	pthread_mutex_unlock(&philo->data->print_mutex);
+}
